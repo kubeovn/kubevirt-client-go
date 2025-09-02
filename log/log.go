81ffbc86acd1a0d31c8d872c9f0704c6bc68a9d4
zhangzujian <zhangzujian.7@gmail.com>
2025-09-02 06:57:29 +0000

    avoid log verbosity flag redefinition
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- log/log.go
+++ log/log.go
@@ -83,6 +83,18 @@ type FilteredLogger struct {
 var Log = DefaultLogger()
 
 func init() {
+	var verbosityFlag bool
+	goflag.VisitAll(func(f *goflag.Flag) {
+		if f.Name == "v" {
+			verbosityFlag = true
+		}
+	})
+
+	if verbosityFlag {
+		// log verbosity flag already defined
+		return
+	}
+
 	// "the practical default level is V(2)"
 	// see https://github.com/kubernetes/community/blob/master/contributors/devel/logging.md
 	goflag.IntVar(&defaultVerbosity, "v", 2, "log level for V logs")
@@ -117,15 +129,17 @@ type NullLogger struct{}
 
 func (n NullLogger) Log(params ...interface{}) error { return nil }
 
-var loggers = make(map[string]*FilteredLogger)
-var defaultComponent = ""
-var defaultVerbosity = 0
+var (
+	loggers          = make(map[string]*FilteredLogger)
+	defaultComponent = ""
+	defaultVerbosity = 0
+)
 
 func createLogger(component string) {
 	lock.Lock()
 	defer lock.Unlock()
 	_, ok := loggers[component]
-	if ok == false {
+	if !ok {
 		logger := klog.NewJSONLogger(os.Stderr)
 		log := MakeLogger(logger)
 		log.component = component
@@ -135,7 +149,7 @@ func createLogger(component string) {
 
 func Logger(component string) *FilteredLogger {
 	_, ok := loggers[component]
-	if ok == false {
+	if !ok {
 		createLogger(component)
 	}
 	return loggers[component]
@@ -236,7 +250,6 @@ func (l FilteredVerbosityLogger) Verbosity(level int) bool {
 }
 
 func (l FilteredLogger) Object(obj LoggableObject) *FilteredLogger {
-
 	name := obj.GetObjectMeta().GetName()
 	namespace := obj.GetObjectMeta().GetNamespace()
 	uid := obj.GetObjectMeta().GetUID()
@@ -340,7 +353,6 @@ func (l FilteredLogger) Criticalf(msg string, args ...interface{}) {
 }
 
 func LogLibvirtLogLine(logger *FilteredLogger, line string) {
-
 	if len(strings.TrimSpace(line)) == 0 {
 		return
 	}
@@ -372,7 +384,7 @@ func LogLibvirtLogLine(logger *FilteredLogger, line string) {
 	pos := strings.TrimSpace(fragments[3])
 	msg := strings.TrimSpace(fragments[4])
 
-	//TODO: implement proper behavior for unsupported GA commands
+	// TODO: implement proper behavior for unsupported GA commands
 	// by either considering the GA version as unsupported or just don't
 	// send commands which not supported
 	if strings.Contains(msg, "unable to execute QEMU agent command") {
@@ -417,7 +429,6 @@ func LogLibvirtLogLine(logger *FilteredLogger, line string) {
 var qemuLogLines = ""
 
 func LogQemuLogLine(logger *FilteredLogger, line string) {
-
 	if len(strings.TrimSpace(line)) == 0 {
 		return
 	}
